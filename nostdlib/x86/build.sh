7aba0593f37073c8d2abce71094f7a456e49c4b6
Junghichi Abo <abo_junghichi@yahoo.co.jp>
2023-09-30 01:05:35 +0900

    nostdlib/x86/build.sh: make wrapper name change easily

--- nostdlib/x86/build.sh
+++ nostdlib/x86/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+STEM=qemu-wrapper-arm
 CC="gcc -Os -Wall -pedantic -Wextra -Wstrict-aliasing=1 \
 	-fno-builtin -nostdlib -static -fno-pie"
-$CC -S qemu-wrapper-arm.c -o /dev/stdout | \
-	sed 's/.rodata/.text/g' >qemu-wrapper-arm.s
+$CC -S $STEM.c -o /dev/stdout | \
+	sed 's/.rodata/.text/g' >$STEM.s
 sh elfheader.sh 0 0 > elfheader.s
-ASM="$CC elfheader.s qemu-wrapper-arm.s -o qemu-wrapper-arm.out"
+ASM="$CC elfheader.s $STEM.s -o $STEM.out"
 $ASM
-TEXT=$(readelf -l qemu-wrapper-arm.out | grep 'LOAD .* R E')
+TEXT=$(readelf -l $STEM.out | grep 'LOAD .* R E')
 ph_cut(){
         cut -f$1 -dx | cut -f1 -d\ 
 }
@@ -16,6 +17,6 @@ sh elfheader.sh $TEXT_ADDR $TEXT_SIZE > elfheader.s
 $ASM
 LENGTH=$(echo $TEXT_SIZE | tr 'abcdef' 'ABCDEF')
 SIZE=$(echo "ibase=16 ; $LENGTH" | bc )
-dd if=qemu-wrapper-arm.out of=qemu-wrapper-arm.out.trunc bs=4096 skip=1
-truncate --size=$SIZE qemu-wrapper-arm.out.trunc
-chmod +x qemu-wrapper-arm.out.trunc
+dd if=$STEM.out of=$STEM.out.trunc bs=4096 skip=1
+truncate --size=$SIZE $STEM.out.trunc
+chmod +x $STEM.out.trunc
